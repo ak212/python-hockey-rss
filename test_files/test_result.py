20de6e795c38cccd0739df4e888b701a3a40160f
ak212 <akoeppel212@gmail.com>
2014-10-11 23:02:59 -0700

    Testing new way of finding the game result.

--- test_files/test_result.py
+++ test_files/test_result.py
@@ -20,25 +20,13 @@ def main():
    link = "http://scores.espn.go.com/nhl/recap?gameId=400564461"
    soup = page_response(link)
    
-   for div in soup.find_all(attrs={"class" : "scoreboard-container"}):
-      away_team = div.find_all(attrs={"class" : "away"})
-      home_team = div.find_all(attrs={"class" : "home"})
-      away_score = div.find_all(attrs={"class" : "awayScore"})
-      home_score = div.find_all(attrs={"class" : "homeScore"})
+   for div in soup.find_all(attrs={"class" : "matchup"}):
+      print div
+#      away_team = div.find_all(attrs={"class" : "away"})
+#      home_team = div.find_all(attrs={"class" : "home"})
+#      away_score = div.find_all(attrs={"class" : "awayScore"})
+#      home_score = div.find_all(attrs={"class" : "homeScore"})
       
-      for aScore in away_score:
-         aNumScore = int(aScore.text)
-      print aNumScore
-      
-      for hScore in home_score:
-         hNumScore = int(hScore.text)
-      print hNumScore
-      
-      if aNumScore > hNumScore:
-         for team in away_team:
-            print team.text
-      else:
-         for team in home_team:
-            print team.text
+
          
 main()
