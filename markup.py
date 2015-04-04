4be3062276db4873c180ab4682b26adf77ea4f43
ak212 <akoeppel212@gmail.com>
2015-04-03 17:52:50 -0700

    Changes to reflect ESPN update

--- markup.py
+++ markup.py
@@ -1,14 +1,22 @@
-'''
-Created on Oct 6, 2014
-
-@author: Aaron
-'''
 import os
+
+__author__ = "Aaron Koeppel"
+__version__ = 1.0
  
 def xml_markup(games, team_ab, team_name):
+   '''Markup the RSS feed using the data obtained.
+
+   :param games: list of games that the team played this season
+   :type games: list of GameData
+   :param team_ab: the team's abbreviated name
+   :type team_ab: string
+   :param team_name: the team's name
+   :type team_name: string'''
+   
    file_name = team_ab + "_feed.xml"
    
-   #Used code from http://stackoverflow.com/questions/7935972/writing-to-a-new-directory-in-python-without-changing-directory
+   '''Used code from http://stackoverflow.com/questions/7935972/
+   writing-to-a-new-directory-in-python-without-changing-directory'''
    
    script_dir = os.path.dirname(os.path.abspath(__file__))
    dest_dir = os.path.join(script_dir, "feeds", team_ab)
@@ -16,7 +24,7 @@ def xml_markup(games, team_ab, team_name):
    try:
       os.makedirs(dest_dir)
    except OSError:
-      pass # already exists\\
+      pass
 
    path = os.path.join(dest_dir, file_name)
    
@@ -24,9 +32,10 @@ def xml_markup(games, team_ab, team_name):
       xml.write('<?xml version="1.0" encoding="UTF-8" ?>\n')
       xml.write("<rss version='2.0'>\n")
       xml.write("<channel>\n")
-      xml.write("<title> " + team_name + " scores</title>\n")
-      xml.write("<description>Latest " + team_name + " scores</description>\n")
-      xml.write("<link>http://espn.go.com/nhl/team/schedule/_/name/" + team_ab + "</link>\n")
+      xml.write("<title>%s scores</title>\n" % team_name)
+      xml.write("<description>Latest %s scores</description>\n" % team_name)
+      xml.write("<link>http://espn.go.com/nhl/team/schedule/_/name/%s</link>\n"
+                % team_ab)
    
       for game in games:
          xml.write("<item>\n")
--- nhl_rss_gen.py
+++ nhl_rss_gen.py
@@ -1,31 +1,33 @@
 # -*- coding: utf-8 -*-
 
-'''
-Created on Oct 6, 2014
-
-@author: Aaron
-'''
+from datetime import date, timedelta
+import logging
+import os
+import re
+import threading
+from time import localtime, strftime, mktime 
 import urllib2
 from bs4 import BeautifulSoup
-import re
-import os
-import logging
-import threading
-from datetime import date, timedelta
-from time import localtime, strftime, mktime
- 
+
 import markup
 import retry_decorator
 
-team_abbrvs = ['ANA','ARI','BOS','BUF','CAR','CBJ','CGY','CHI','COL','DAL',
-              'DET','EDM','FLA','LA','MIN','MTL','NSH','NJ','NYI','NYR', 
-              'OTT','PHI','PIT','SJ','STL','TB','TOR','VAN','WPG','WSH']
 
-team_names = ['Ducks','Coyotes','Bruins','Sabres','Hurricanes','Blue Jackets', 
-              'Flames','Blackhawks','Avalanche','Stars','Red Wings','Oilers', 
-              'Panthers','Kings','Wild','Canadiens','Predators','Devils', 
-              'Islanders','Rangers','Senators','Flyers','Penguins','Sharks', 
-              'Blues','Lightning','Maple Leafs','Canucks','Jets','Capitals']
+__author__ = "Aaron Koeppel"
+__version__ = 1.11
+__modified__ = '4/3/15'
+
+team_abbrvs = ['ANA', 'ARI', 'BOS', 'BUF', 'CAR', 'CBJ', 'CGY', 'CHI', 'COL', 'DAL',
+              'DET', 'EDM', 'FLA', 'LA', 'MIN', 'MTL', 'NSH', 'NJ', 'NYI', 'NYR',
+              'OTT', 'PHI', 'PIT', 'SJ', 'STL', 'TB', 'TOR', 'VAN', 'WPG', 'WSH']
+
+team_names = ['Ducks', 'Coyotes', 'Bruins', 'Sabres', 'Hurricanes', 'Blue Jackets',
+              'Flames', 'Blackhawks', 'Avalanche', 'Stars', 'Red Wings', 'Oilers',
+              'Panthers', 'Kings', 'Wild', 'Canadiens', 'Predators', 'Devils',
+              'Islanders', 'Rangers', 'Senators', 'Flyers', 'Penguins', 'Sharks',
+              'Blues', 'Lightning', 'Maple Leafs', 'Canucks', 'Jets', 'Capitals']
+
+total_games = 0
 
 file_name = str(date.today()) + '.log'
 
@@ -45,22 +47,41 @@ logging.basicConfig(filename=path,
 logger = logging.getLogger(__name__)
 
 class GameData(object):
+   '''Class to represent a game played by a hockey team.
+
+   :param link: html link to the game recap from ESPN.com
+   :type link: string
+   :param headline: the headline from the game recap
+   :type headline: string
+   :param date: the date the game was played
+   :type date: string'''
+   
    def __init__(self, link, headline, date):
+      self.id = total_games
       self.link = link
       self.headline = headline
       self.date = date
       self.result = None
       
    def char_convert_link(self):
+      '''Convert ampersands from the character '&' to the character encoding 
+      '&amp;' to properly request html'''
+      
       self.link = re.sub('[&]', '&amp;', self.link)
       
    def print_game_data(self):
-      print self.headline, self.link, self.date, self.result
+      '''Print data of the class'''
+      
+      print self.id, self.headline, self.link, self.date, self.result
       
    def list_data(self):
+      '''Return the data of the class as a list'''
       return [self.headline, self.link, self.date, self.result]
    
-   def modify_headline(self, link):
+   def modify_headline(self):
+      '''Change the headline to also include the game date and the result of
+      the game'''
+      
       headline = self.headline
       
       if self.date[4] == "0":
@@ -70,10 +91,22 @@ class GameData(object):
       try:
          self.headline = self.headline + self.result + " - " + headline
       except TypeError:
-         logger.debug("TypeError from: " + link)
-         self.headline = self.headline + self.result + " No Headline"
+         logger.debug("TypeError from: " + self.link)
+         try:
+            self.headline = self.headline + self.result + " - No Headline"
+         except TypeError:
+            self.headline = self.headline + " - " + headline
          
-   def find_winner(self, team_name, soup, link):
+         
+   def find_winner(self, team_name, soup):
+      '''Find the winner of the hockey game through the game recap.
+   
+      :param team_name: the name of the hockey team; used to check if this team
+      was the winner
+      :type team_name: string
+      :param soup: the source file of the recap page
+      :type soup: string'''
+      
       matchup = soup.find(class_="matchup")
       home = matchup.find(class_="team home")
       home_team = str(home.find('a').text)
@@ -93,18 +126,32 @@ class GameData(object):
       elif home_score < away_score:
          result = "W"
       else:
-         logger.debug("Error determining winner from: " + link)
+         logger.debug("Error determining winner from: " + self.link)
          result = ""
 
       self.result = result
    
 def page_response(link):
+   '''Retrieve the source code of the page
+
+   :param link: the link to the page that will be scraped
+   :type link: string'''
+   
+#   logger.debug("Requesting: " + link)
    response = urlopen_with_retry(link)
    page_source = response.read()
    
    return BeautifulSoup(page_source)
 
 def thread_jumpoff(team_ab, team_name):
+   '''Intermediary function between the main function and the game extraction
+   and xml generation
+
+   :param team_ab: the team's abbreviated name
+   :type team_ab: string
+   :param team_name: the team's name
+   :type team_name: string'''
+   
    games = extract_game_data(team_ab, team_name)
    games.sort(key=lambda x: x.date, reverse=True)
      
@@ -114,26 +161,41 @@ def thread_jumpoff(team_ab, team_name):
                " completed with " + str(len(games)) + " games logged")
 
 def extract_game_data(team_ab, team_name):
+   '''Extract the game data (date, headline, result) for each game the team 
+   has played.
+
+   :param team_ab: the team's abbreviated name
+   :type team_ab: string
+   :param team_name: the team's name
+   :type team_name: string'''
+   
    games = []
    link = "http://espn.go.com/nhl/team/schedule/_/name/" + team_ab
    soup = page_response(link)
+   global total_games
    
    for div in soup.find_all(attrs={"class" : "score"}):
-      complete_link = "http://espn.go.com" + str(
-                                                 div.find('a').get('href').
-                                                 encode('utf-8', 'ignore'))
+      link_ending = str(div.find('a').get('href').encode('utf-8', 'ignore'))
+      recap_link = "http://espn.go.com" + link_ending
+      boxscore_link = "http://espn.go.com/nhl/boxscore?gameId=" + recap_link[36:]
+      
+      recap_link_soup = page_response(recap_link)
+      game_headline = get_game_headline(recap_link_soup, recap_link)
+      
+      boxscore_link_soup = page_response(boxscore_link)
+      game_date = get_game_date(boxscore_link_soup)
 
-      complete_link_soup = page_response(complete_link)
-      game_headline = get_game_headline(complete_link_soup, complete_link)
-      game_date = get_game_date(complete_link_soup)
-
-      new_game = GameData(complete_link, game_headline, game_date)
+      new_game = GameData(recap_link, game_headline, game_date)
+      total_games += 1
       
       new_game.char_convert_link()
-      new_game.find_winner(team_name, complete_link_soup, complete_link)
-      new_game.modify_headline(complete_link)
+      new_game.find_winner(team_name, boxscore_link_soup)
+      new_game.modify_headline()
       
+#      new_game.print_game_data()
+
       games.append(new_game)
+      
       
    return games
 
@@ -142,6 +204,14 @@ def urlopen_with_retry(link):
    return urllib2.urlopen(link)
 
 def get_game_headline(soup, link):
+   '''Extract the headline from the page source.
+
+   :param soup: the source file of the recap page
+   :type soup: string
+   :param link: the link to the page that will was scraped; passed in case of 
+   error for logging
+   :type link: string'''
+   
    try:
       for meta in soup.findAll('meta', {"property":'og:title'}):
          return meta.get('content')
@@ -149,10 +219,15 @@ def get_game_headline(soup, link):
       logger.debug('There was an error with the request from: ' + link)
       
 def get_game_date(soup):
-   for div in soup.find_all(attrs={"class" : "scoreboard-strip-wrapper"}):
-      date_string = str(div.find('a').get('href').encode('utf-8', 'ignore'))
+   '''Extract the headline from the page source.
 
-      return date_string[21:]
+   :param soup: the source file of the recap page
+   :type soup: string'''
+   
+   for item in soup.find_all('a'):
+      if '/nhl/scoreboard?date=' in item.get('href'):
+         return item.get('href')[21:]
+
       
 def main():
    start_time = localtime()
@@ -173,8 +248,9 @@ def main():
    
    finish_time = localtime()
    logger.info("Finish time: " + strftime("%d-%b-%Y %H:%M:%S ", finish_time))
+   logger.info("Total games: " + str(total_games))
    logger.info("Total time: " + 
-               str(timedelta(seconds=mktime(finish_time)-mktime(start_time))))
+               str(timedelta(seconds=mktime(finish_time) - mktime(start_time))))
    
 if __name__ == '__main__':
    main()
