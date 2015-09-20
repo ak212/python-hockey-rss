45464ace2b90a5bacd0d4d45655fdcf22d36a3d0
ak212 <akoeppel212@gmail.com>
2015-09-19 17:39:55 -0700

    Minor refactoring

--- nhl_rss_gen.py
+++ nhl_rss_gen.py
@@ -7,15 +7,17 @@ import re
 import threading
 from time import localtime, strftime, mktime 
 import urllib2
+
 from bs4 import BeautifulSoup
 
+import GameData
 import markup
 import retry_decorator
 
 
 __author__ = "Aaron Koeppel"
-__version__ = 1.11
-__modified__ = '4/3/15'
+__version__ = 1.12
+__modified__ = '9/18/15'
 
 team_abbrvs = ['ANA', 'ARI', 'BOS', 'BUF', 'CAR', 'CBJ', 'CGY', 'CHI', 'COL', 'DAL',
               'DET', 'EDM', 'FLA', 'LA', 'MIN', 'MTL', 'NSH', 'NJ', 'NYI', 'NYR',
@@ -45,91 +47,6 @@ logging.basicConfig(filename=path,
                     format='(%(threadName)-10s) %(message)s',
                     level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-
-class GameData(object):
-   '''Class to represent a game played by a hockey team.
-
-   :param link: html link to the game recap from ESPN.com
-   :type link: string
-   :param headline: the headline from the game recap
-   :type headline: string
-   :param date: the date the game was played
-   :type date: string'''
-   
-   def __init__(self, link, headline, date):
-      self.id = total_games
-      self.link = link
-      self.headline = headline
-      self.date = date
-      self.result = None
-      
-   def char_convert_link(self):
-      '''Convert ampersands from the character '&' to the character encoding 
-      '&amp;' to properly request html'''
-      
-      self.link = re.sub('[&]', '&amp;', self.link)
-      
-   def print_game_data(self):
-      '''Print data of the class'''
-      
-      print self.id, self.headline, self.link, self.date, self.result
-      
-   def list_data(self):
-      '''Return the data of the class as a list'''
-      return [self.headline, self.link, self.date, self.result]
-   
-   def modify_headline(self):
-      '''Change the headline to also include the game date and the result of
-      the game'''
-      
-      headline = self.headline
-      
-      if self.date[4] == "0":
-         self.headline = "[" + self.date[5:6] + "/" + self.date[6:] + "] "
-      else:
-         self.headline = "[" + self.date[4:6] + "/" + self.date[6:] + "] "
-      try:
-         self.headline = self.headline + self.result + " - " + headline
-      except TypeError:
-         logger.debug("TypeError from: " + self.link)
-         try:
-            self.headline = self.headline + self.result + " - No Headline"
-         except TypeError:
-            self.headline = self.headline + " - " + headline
-         
-         
-   def find_winner(self, team_name, soup):
-      '''Find the winner of the hockey game through the game recap.
-   
-      :param team_name: the name of the hockey team; used to check if this team
-      was the winner
-      :type team_name: string
-      :param soup: the source file of the recap page
-      :type soup: string'''
-      
-      matchup = soup.find(class_="matchup")
-      home = matchup.find(class_="team home")
-      home_team = str(home.find('a').text)
-      home_score = int(home.find(class_="gp-homeScore").text)
-      away_score = int(matchup.
-                       find(class_="team away").
-                       find(class_="gp-awayScore").text)
-                     
-      home = home_team == team_name
-      
-      if home_score > away_score and home:
-         result = "W"
-      elif home_score < away_score and home:
-         result = "L"
-      elif home_score > away_score:
-         result = "L"
-      elif home_score < away_score:
-         result = "W"
-      else:
-         logger.debug("Error determining winner from: " + self.link)
-         result = ""
-
-      self.result = result
    
 def page_response(link):
    '''Retrieve the source code of the page
